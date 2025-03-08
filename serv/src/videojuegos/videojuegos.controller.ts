83809a8900e3ae1f63695e23ca9fa7fc2d8d10b0
cristianasir2 <manzcris@gmail.com>
2025-03-08 19:39:06 +0100

    Utilizacion de token para la autenticacion de usuarios

--- serv/src/videojuegos/videojuegos.controller.ts
+++ serv/src/videojuegos/videojuegos.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { CreateVideojuegoDto } from './dto/create-videojuego.dto';
 import { UpdateVideojuegoDto } from './dto/update-videojuego.dto';
 import { VideojuegoService } from './videojuegos.service';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 
 @Controller('videojuegos')
 export class VideojuegosController {
   constructor(private readonly videojuegosService: VideojuegoService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createVideojuegoDto: CreateVideojuegoDto) {
     return this.videojuegosService.create(createVideojuegoDto);
   }
@@ -23,11 +25,13 @@ export class VideojuegosController {
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateVideojuegoDto: UpdateVideojuegoDto) {
     return this.videojuegosService.update(+id, updateVideojuegoDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.videojuegosService.remove(+id);
   }
