4092de06f1cf54c13c33f8903cc2a350877177fa
TxusGamer027 <juanlusanchez27@gmail.com>
2025-03-10 10:15:20 +0100

    Añado UseGuards a los controladores que faltaban

--- serv/src/cesta/cesta.controller.ts
+++ serv/src/cesta/cesta.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { CestaService } from './cesta.service';
 import { CreateCestaDto } from './dto/create-cesta.dto';
 import { UpdateCestaDto } from './dto/update-cesta.dto';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 
 @Controller('cesta')
 export class CestaController {
   constructor(private readonly cestaService: CestaService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createCestaDto: CreateCestaDto) {
     return this.cestaService.create(createCestaDto);
   }
@@ -23,11 +25,13 @@ export class CestaController {
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateCestaDto: UpdateCestaDto) {
     return this.cestaService.update(+id, updateCestaDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.cestaService.remove(+id);
   }
--- serv/src/cliente/cliente.controller.ts
+++ serv/src/cliente/cliente.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { ClienteService } from './cliente.service';
 import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 
 @Controller('cliente')
 export class ClienteController {
   constructor(private readonly clienteService: ClienteService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createClienteDto: CreateClienteDto) {
     return this.clienteService.create(createClienteDto);
   }
@@ -23,11 +25,13 @@ export class ClienteController {
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateClienteDto: UpdateClienteDto) {
     return this.clienteService.update(+id, updateClienteDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.clienteService.remove(+id);
   }
