07b5c182c7b7a06d3bf98cad323577866ef74514
cristianasir2 <manzcris@gmail.com>
2025-03-14 10:07:26 +0100

    .

--- serv/dist/auth/local-strategy/local-strategy.d.ts
+++ serv/dist/auth/local-strategy/local-strategy.d.ts
@@ -1,7 +1,7 @@
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
 import { CreateClienteDto } from 'src/cliente/dto/create-cliente.dto';
-declare const LocalStrategy_base: new (...args: [] | [options: import("passport-local").IStrategyOptionsWithRequest] | [options: import("passport-local").IStrategyOptions]) => Strategy & {
+declare const LocalStrategy_base: new (...args: [options: import("passport-local").IStrategyOptionsWithRequest] | [options: import("passport-local").IStrategyOptions] | []) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class LocalStrategy extends LocalStrategy_base {
