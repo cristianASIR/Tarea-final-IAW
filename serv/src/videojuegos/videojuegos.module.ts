ce8b84aa85d0939774e03591dc5d6109aa004cb8
TxusGamer027 <juanlusanchez27@gmail.com>
2025-03-14 19:04:14 +0100

    añado servicio para introducir juagos automaticamente en la base de datos

--- serv/src/videojuegos/videojuegos.module.ts
+++ serv/src/videojuegos/videojuegos.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Videojuego } from './entities/videojuego.entity';
 import { VideojuegoService } from './videojuegos.service';
 import { VideojuegosController } from './videojuegos.controller';
+import { SeedService } from './seed.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Videojuego])],
   controllers: [VideojuegosController],
-  providers: [VideojuegoService],
+  providers: [VideojuegoService, SeedService],
 })
 export class VideojuegosModule {}
